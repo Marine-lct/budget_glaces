5a4e82a5798b9e37a045bded2285a8dbd0b2ae85
Marine lct <marine.lancelot06@gmail.com>
2023-11-27 10:57:18 +0100

    Création de variables, tests, calculs

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const app = express();
 
 const price = 2.50;
 const campers = 15;
+let taxRate = 0.08;
+const budget = 40;
+let total = price * campers * (1 + taxRate); //inclus la taxe
+
+let totalExceedsBudget = total > budget; // Si le total depasse le budget
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>Valeurs des variables</h1>
     <p>Price: ${price}</p>
     <p>Campers: ${campers}</p>
+    <p>La taxe de vente est de 8 %: ${taxRate}</p>
+    <p>Budget: ${budget}</p>
+    <p>Le total dépasse t'il le budget ? : ${totalExceedsBudget}</p>
+    <p>Total avec la taxe: ${total}</p>
   `);
 });
 
