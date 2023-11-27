9a146428d8b9ba9fb966e3e19ff34672d6eab6cc
Marine lct <marine.lancelot06@gmail.com>
2023-11-27 10:23:56 +0100

    Mise en place du projet node.js, express et déclaration des variables price, campers

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello, world!</h1>');
+const price = 2.50;
+const campers = 15;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Valeurs des variables</h1>
+    <p>Price: ${price}</p>
+    <p>Campers: ${campers}</p>
+  `);
 });
 
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
 });
---
+++ script.js
@@ -0,0 +1,5 @@
+const price = 2.50;
+const campers = 15;
+
+console.log(price);
+console.log(campers);
